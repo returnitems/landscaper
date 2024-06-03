5fea46c82b79517324f43556fb7c5675cb4ccd77
returnitems <diwan.umang@gmail.com>
2024-06-02 21:05:12 -0400

    started work on game function

---
+++ game.py
@@ -0,0 +1,17 @@
+import random
+
+print('Welcome to Landscaper two point oh!')
+user_name = input('Please enter your name: ')
+print('Hello' + user_name + '! Shall we get started?')
+start_input = input('Type 1 for yes, 2 for no: ')
+start_input = int(start_input)
+
+# Game function
+def gameStart():
+    start_cash = 0
+    
+
+if start_input == 2:
+    print("That's sad but understandable. Have a good day!")
+else:
+    print('Great! We will now begin our landscaping journey. Unfortunately, for now we only have our teeth to work with.')
